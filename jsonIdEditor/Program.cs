d59b1ac681c4b7e361d9fd10dfabbb22db034afe
Albert Marsnik <Amarsni@eu.trimblecorp.net>
2018-03-09 10:30:54 +0100

    Add project files.

---
+++ jsonIdEditor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace jsonIdEditor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            var j = new jsonFR();
+            Console.WriteLine("Find: ");
+            var find = Console.ReadLine();
+            Console.WriteLine("Replace With: ");
+            var replace = Console.ReadLine();
+
+            var result = j.EditorialResponse(find, replace, " (Updated).json");
+            Console.WriteLine(result);
+
+            
+        }
+    }
+}
---
+++ jsonIdEditor/jsonFR.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace jsonIdEditor
+{
+    class jsonFR
+    {
+        public int EditorialResponse(string word, string replacement, string saveFileName)
+        {
+            string fullFilePath;
+            OpenFileDialog file = new OpenFileDialog();
+            try
+            {
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    fullFilePath = file.FileName;
+                    StreamReader reader = new StreamReader(fullFilePath);
+                    string input = reader.ReadToEnd();
+                    var dirFn = fullFilePath.Split('.');
+                    var directory = dirFn[0];
+
+                    using (StreamWriter writer = new StreamWriter(directory + saveFileName, true))
+                    {
+                        {
+                            string output = input.Replace(word, replacement);
+                            writer.Write(output);
+                        }
+                        writer.Close();
+
+                    }
+                    return 0;
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return 1;
+        }
+    }
+}
