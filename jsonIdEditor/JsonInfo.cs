0b8da3417804b617222525f1cb9ad1b0d7580bc2
Albert Marsnik <acmarsnik@hotmail.com>
2018-03-13 17:02:15 +0100

    Refactored attributes from JsonFR to JsonInfo

--- jsonIdEditor/JsonInfo.cs
+++ jsonIdEditor/JsonInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,35 @@ namespace JsonIdEditor
         public string fullFilePath;
         public int fileCnt = 0;
         public OpenFileDialog file = new OpenFileDialog();
+        public string strAns;
+        public string strAns2;
+        public int intAns;
+        public Replacer rep = new Replacer();
+        public StreamReader reader;
+        public string orgJsonTxt;
+        public String[] dirFn;
+        public string directory;
+        public string updatedFilePath;
+        public String[] orgJsonObjects;
+        public string akRe = "(\\{.*?\"ArticleKey\":.*?,)";
+        public string onRe = "(\"OrderNumber\":.*?,)";
+        public string vknRe = "(\"VKN\":.*?,)";
+        public string ctRe = "(\"CatalogType\":.*?,)";
+        public string cidRe = "(\"CatalogID\":.*?,)";
+        public string lcidRe = "(\"Lcid\":.*?,)";
+        public string sidRe = "(\"SupplierID\":.*?,)";
+        public string id = "";
+        public string akIdPrefix = "\"ak-";
+        public string onIdPrefix = "-on-";
+        public string vknIdPrefix = "-vkn-";
+        public string ctIdPrefix = "-ct-";
+        public string cidIdPrefix = "-cid-";
+        public string lcidIdPrefix = "-lcid-";
+        public string sidIdPrefix = "-sid-";
+        public string idSuffix = "\": ";
+        public int prevArtKey3Dig = 000;
+        public int currArtKey3Dig = 000;
+        public bool newCatalog = false;
+        public RegexMatcher rm = new RegexMatcher();
     }
 }
--- jsonIdEditor/jsonFR.cs
+++ jsonIdEditor/jsonFR.cs
@@ -24,65 +24,43 @@ namespace JsonIdEditor
                 {
                     Console.WriteLine("Do you want to split the file by the first 3 digits of the Article Key?" + Environment.NewLine + 
                         "Enter y for yes or n for no");
-                    string strAns = Console.ReadLine();
+                    jsonInfo.strAns = Console.ReadLine();
 
-                    int intAns = 0;
+                    jsonInfo.intAns = 0;
 
-                    if (strAns == "y" || strAns == "Y" || strAns == "Yes" || strAns == "YES" || strAns == "yes")
+                    if (jsonInfo.strAns == "y" || jsonInfo.strAns == "Y" || jsonInfo.strAns == "Yes" || jsonInfo.strAns == "YES" || jsonInfo.strAns == "yes")
                     {
-                        intAns = 1;
+                        jsonInfo.intAns = 1;
                         Console.WriteLine("Do you want all of the Catalogs in 1 file or separate files?" + Environment.NewLine +
                         "Enter 1 for 1 file or s for seperate");
-                        string strAns2 = Console.ReadLine();
-                        if (strAns2.Trim() != "1")
+                        jsonInfo.strAns2 = Console.ReadLine();
+                        if (jsonInfo.strAns2.Trim() != "1")
                         {
-                            intAns = 2;
+                            jsonInfo.intAns = 2;
                         }
 
 
                     }
 
                     jsonInfo.fullFilePath = jsonInfo.file.FileName;
-                    Replacer rep = new Replacer();
-                    StreamReader reader = new StreamReader(jsonInfo.fullFilePath);
-                    string input = reader.ReadToEnd();
-                    String[] dirFn = jsonInfo.fullFilePath.Split('.');
-                    string directory = dirFn[0];
-                    string updatedFilePath = directory + " (Updated)" + ".json";
-                    String[] objects = input.Split('}');
+                    jsonInfo.reader = new StreamReader(jsonInfo.fullFilePath);
+                    jsonInfo.orgJsonTxt = jsonInfo.reader.ReadToEnd();
+                    jsonInfo.dirFn = jsonInfo.fullFilePath.Split('.');
+                    jsonInfo.directory = jsonInfo.dirFn[0];
+                    jsonInfo.updatedFilePath = jsonInfo.directory + " (Updated)" + ".json";
+                    jsonInfo.orgJsonObjects = jsonInfo.orgJsonTxt.Split('}');
 
-                    string akRe = "(\\{.*?\"ArticleKey\":.*?,)";
-                    string onRe = "(\"OrderNumber\":.*?,)";
-                    string vknRe = "(\"VKN\":.*?,)";
-                    string ctRe = "(\"CatalogType\":.*?,)";
-                    string cidRe = "(\"CatalogID\":.*?,)";
-                    string lcidRe = "(\"Lcid\":.*?,)";
-                    string sidRe = "(\"SupplierID\":.*?,)";
-                    string id = "";
-                    string akIdPrefix = "\"ak-";
-                    string onIdPrefix = "-on-";
-                    string vknIdPrefix = "-vkn-";
-                    string ctIdPrefix = "-ct-";
-                    string cidIdPrefix = "-cid-";
-                    string lcidIdPrefix = "-lcid-";
-                    string sidIdPrefix = "-sid-";
-                    string idSuffix = "\": ";
-                    int prevArtKey3Dig = 000;
-                    int currArtKey3Dig = 000;
-
-                    bool newCatalog = false;
-
-                    RegexMatcher rm = new RegexMatcher();
                     string updatedJson = "";
-                    foreach(string s in objects) {
-                        string akAtt = rm.MatchRegex(s, akRe);
-                        string onAtt = rm.MatchRegex(s, onRe);
-                        string vknAtt = rm.MatchRegex(s, vknRe);
-                        string ctAtt = rm.MatchRegex(s, ctRe);
-                        string cidAtt = rm.MatchRegex(s, cidRe);
-                        string lcidAtt = rm.MatchRegex(s, lcidRe);
-                        string sidAtt = rm.MatchRegex(s, sidRe);
-                        id = "";
+
+                    foreach(string s in jsonInfo.orgJsonObjects) {
+                        string akAtt = jsonInfo.rm.MatchRegex(s, jsonInfo.akRe);
+                        string onAtt = jsonInfo.rm.MatchRegex(s, jsonInfo.onRe);
+                        string vknAtt = jsonInfo.rm.MatchRegex(s, jsonInfo.vknRe);
+                        string ctAtt = jsonInfo.rm.MatchRegex(s, jsonInfo.ctRe);
+                        string cidAtt = jsonInfo.rm.MatchRegex(s, jsonInfo.cidRe);
+                        string lcidAtt = jsonInfo.rm.MatchRegex(s, jsonInfo.lcidRe);
+                        string sidAtt = jsonInfo.rm.MatchRegex(s, jsonInfo.sidRe);
+                        jsonInfo.id = "";
                         String[] aks = akAtt.Split(':');
                         String[] ons = onAtt.Split(':');
                         String[] vs = vknAtt.Split(':');
@@ -96,73 +74,73 @@ namespace JsonIdEditor
                             artKey1 = artKey1.Replace('\\', ' ');
                             string artKey = artKey1.Trim();
                             string[] ArtKey3Dig1 = artKey.Split('.');
-                            if(intAns == 1 || intAns == 2) {
-                                currArtKey3Dig = int.Parse(ArtKey3Dig1[0]);
+                            if(jsonInfo.intAns == 1 || jsonInfo.intAns == 2) {
+                                jsonInfo.currArtKey3Dig = int.Parse(ArtKey3Dig1[0]);
 
-                                if (currArtKey3Dig != prevArtKey3Dig && prevArtKey3Dig != 000){
-                                        newCatalog = true;
+                                if (jsonInfo.currArtKey3Dig != jsonInfo.prevArtKey3Dig && jsonInfo.prevArtKey3Dig != 000){
+                                        jsonInfo.newCatalog = true;
                                     }
                             }
-                            id += akIdPrefix;
-                            id += artKey;
+                            jsonInfo.id += jsonInfo.akIdPrefix;
+                            jsonInfo.id += artKey;
                         }
                         if (ons.Length > 1){
                             string ordNum1 = ons[1].Replace(',', ' ');
                             ordNum1 = ordNum1.Replace('"', ' ');
                             ordNum1 = ordNum1.Replace('\\', ' ');
                             string ordNum = ordNum1.Trim();
-                            id += onIdPrefix;
-                            id += ordNum;
+                            jsonInfo.id += jsonInfo.onIdPrefix;
+                            jsonInfo.id += ordNum;
                         }
                         if (vs.Length > 1){
                             string vkn1 = vs[1].Replace(',', ' ');
                             vkn1 = vkn1.Replace('"', ' ');
                             vkn1 = vkn1.Replace('\\', ' ');
                             string vkn = vkn1.Trim();
-                            id += vknIdPrefix;
-                            id += vkn;
+                            jsonInfo.id += jsonInfo.vknIdPrefix;
+                            jsonInfo.id += vkn;
                         }
                         if (cts.Length > 1){
                             string ct1 = cts[1].Replace(',', ' ');
                             ct1 = ct1.Replace('"', ' ');
                             ct1 = ct1.Replace('\\', ' ');
                             string ct = ct1.Trim();
-                            id += ctIdPrefix;
-                            id += ct;
+                            jsonInfo.id += jsonInfo.ctIdPrefix;
+                            jsonInfo.id += ct;
                         }
                         if (cids.Length > 1){
                             string cid1 = cids[1].Replace(',', ' ');
                             cid1 = cid1.Replace('"', ' ');
                             cid1 = cid1.Replace('\\', ' ');
                             string cid = cid1.Trim();
-                            id += cidIdPrefix;
-                            id += cid;
+                            jsonInfo.id += jsonInfo.cidIdPrefix;
+                            jsonInfo.id += cid;
                         }
                         if (lcids.Length > 1){
                             string lcid1 = lcids[1].Replace(',', ' ');
                             lcid1 = lcid1.Replace('"', ' ');
                             lcid1 = lcid1.Replace('\\', ' ');
                             string lcid = lcid1.Trim();
-                            id += lcidIdPrefix;
-                            id += lcid;
+                            jsonInfo.id += jsonInfo.lcidIdPrefix;
+                            jsonInfo.id += lcid;
                         }
                         if (sids.Length > 1){
                             string sid1 = sids[1].Replace(',', ' ');
                             sid1 = sid1.Replace('"', ' ');
                             sid1 = sid1.Replace('\\', ' ');
                             string sid = sid1.Trim();
-                            id += sidIdPrefix;
-                            id += sid;
-                            id += idSuffix;
-                            id += akAtt;
+                            jsonInfo.id += jsonInfo.sidIdPrefix;
+                            jsonInfo.id += sid;
+                            jsonInfo.id += jsonInfo.idSuffix;
+                            jsonInfo.id += akAtt;
                         }
                         
                         if(akAtt != string.Empty) {
-                            if(newCatalog == true)
+                            if(jsonInfo.newCatalog == true)
                             {
                                 updatedJson += Environment.NewLine + "}" + Environment.NewLine + "}";
 
-                                if (intAns == 1)
+                                if (jsonInfo.intAns == 1)
                                 {
                                     updatedJson += Environment.NewLine + Environment.NewLine + Environment.NewLine + Environment.NewLine + Environment.NewLine;
                                     updatedJson += "////BREAK BETWEEN CONTENT THAT SHOULD BE USED IN TOPAZ";
@@ -170,14 +148,14 @@ namespace JsonIdEditor
                                 }
                                 else
                                 {
-                                    rep.ReplaceMany(directory + "-" + prevArtKey3Dig.ToString() + ".json", updatedJson);
+                                    jsonInfo.rep.ReplaceMany(jsonInfo.directory + "-" + jsonInfo.prevArtKey3Dig.ToString() + ".json", updatedJson);
                                     jsonInfo.fileCnt += 1;
                                     updatedJson = "";
                                 }
-                                updatedJson += rep.getFileStart();
+                                updatedJson += jsonInfo.rep.getFileStart();
                             }
-                            string newObject = s.Replace(akAtt, id);
-                            if (newCatalog == true)
+                            string newObject = s.Replace(akAtt, jsonInfo.id);
+                            if (jsonInfo.newCatalog == true)
                             {
                                 newObject = newObject.Substring(1);
                             }
@@ -191,19 +169,19 @@ namespace JsonIdEditor
                             
                         }
 
-                        prevArtKey3Dig = currArtKey3Dig;
-                        newCatalog = false;
+                        jsonInfo.prevArtKey3Dig = jsonInfo.currArtKey3Dig;
+                        jsonInfo.newCatalog = false;
 
 
                     }
 
-                    if(intAns == 0 || intAns == 1)
+                    if(jsonInfo.intAns == 0 || jsonInfo.intAns == 1)
                     {
-                        rep.ReplaceMany(updatedFilePath, updatedJson);
+                        jsonInfo.rep.ReplaceMany(jsonInfo.updatedFilePath, updatedJson);
                     }
                     else
                     {
-                        rep.ReplaceMany(directory + "-" + prevArtKey3Dig.ToString() + ".json", updatedJson);
+                        jsonInfo.rep.ReplaceMany(jsonInfo.directory + "-" + jsonInfo.prevArtKey3Dig.ToString() + ".json", updatedJson);
                     }
                     
 
